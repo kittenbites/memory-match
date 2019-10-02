78dd8a7b23925e318fcff8d481fb96d6f32c0747
Samantha Nagle <sammie.nagle@gmail.com>
2019-10-02 16:00:38 -0700

    Implemented card matching Can now match two cards, two unmatched cards will now flip over again

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,43 @@ $(document).ready(initializeApp);
 
 function initializeApp(){
 
-$(".card_div").on("click",".card_back",handleCardClick);
+$(".container").on("click",".card_div",handleCardClick);
 
 }
 
+var firstCardClicked = null;
+var secondCardClicked = null;
+var matches = null;
+
 
 function handleCardClick(event){
-  $(event.currentTarget).toggleClass("hidden");
+  var currentCard = $(event.currentTarget);
+  $(currentCard).find(".card_back").toggleClass("hidden");
+  if(!firstCardClicked){
+    firstCardClicked = currentCard;
+  }
+  else if(!secondCardClicked){
+    secondCardClicked = currentCard;
+    var cardFront1 = firstCardClicked.find(".card_front").css("background-image");
+    var cardFront2 = secondCardClicked.find(".card_front").css("background-image");
+
+    if (cardFront1 === cardFront2){
+      console.log("It's a match!");
+      matches++;
+    } else {
+      console.log("Try again!");
+      setTimeout(function(){
+      firstCardClicked.find(".card_back").toggleClass("hidden");
+      secondCardClicked.find(".card_back").toggleClass("hidden");
+      resetCards();
+    },1500)
+    }
+  }
+  }
+
+
+
+function resetCards(){
+  firstCardClicked = null;
+  secondCardClicked = null;
 }
